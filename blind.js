ffa4092aee50e64375aee1f12b8171b082d8ca30
zombie <zombie@users.noreply.github.com>
2017-06-18 19:01:21 +0200

    Add support for splinter review page

--- blind.js
+++ blind.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const {$, BUGZILLA: {bug_id, user}} = window.wrappedJSObject;
+const {$, BUGZILLA, Splinter} = window.wrappedJSObject;
+const bug_id = BUGZILLA.bug_id || Splinter.bugId;
+const user = BUGZILLA.user;
 
 async function storage(id, data = {}) {
   const bug = (await browser.storage.local.get({[id]: {}}))[id];
@@ -47,21 +49,25 @@ function submitter() {
   }
 }
 
+function icon() {
+  const img = document.createElement("img");
+  img.src = browser.runtime.getURL("icon.png");
+  img.title = "Blind Reviews";
+  img.className = "br-icon";
+  return img;
+}
+
 // Augment any mention of the author across the bug page.
 function augment(author) {
   const vcards = document.querySelectorAll(`.${author}`);
   for (const vcard of vcards) {
-    const icon = document.createElement("img");
-    icon.src = browser.runtime.getURL("icon.png");
-    icon.title = "Blind Reviews";
-    icon.className = "br-icon";
-
-    vcard.parentElement.insertBefore(icon, vcard);
+    const img = icon();
+    vcard.parentElement.insertBefore(img, vcard);
     vcard.classList.toggle("br-vcard", true);
   }
 }
 
-async function init() {
+async function show_bug() {
   const author = submitter();
   const bug = await storage(bug_id, {author});
   if (bug.author) {
@@ -71,6 +77,49 @@ async function init() {
     // The people module summary reveals assignee.
     document.getElementById("module-people-subtitle").innerHTML = "";
   }
+}
+
+async function splinter() {
+  const bug = await storage(bug_id);
+  const ac = document.querySelector("#attachCreator");
+  const br = document.querySelector("#bugReporter");
+  const mentions = [ac];
+
+  if (ac.textContent === br.textContent) {
+    mentions.push(br);
+  }
+
+  const flags = document.querySelectorAll(`#flags span[title="${ac.textContent}"]`);
+  for (const f of flags) {
+    mentions.push(f);
+  }
+
+  for (const m of mentions) {
+    const img = icon();
+    const fn = document.createElement("span");
+    fn.className = "br-r";
+
+    m.parentNode.insertBefore(img, m);
+    m.parentNode.insertBefore(fn, m);
+    m.parentNode.classList.toggle("br-vcard", true);
+  }
+
+  const comments = document.querySelectorAll("#patchIntro>.pre-wrap");
+  for (const c of comments) {
+    if (!bug.visible && c.textContent.startsWith("# User ")) {
+      c.textContent = "# User [redacted]";
+    }
+  }
+
+  setVisible(bug.visible);
+}
+
+function init() {
+  const PAGES = {
+    "/show_bug.cgi": show_bug,
+    "/page.cgi": window.addEventListener("load", splinter),
+  };
+  PAGES[window.location.pathname]();
 }
 
 init();

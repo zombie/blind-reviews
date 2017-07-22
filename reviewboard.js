15009b767f662c4558d3688c5e8825da35216683
zombie <zombie@users.noreply.github.com>
2017-07-23 01:49:39 +0200

    Conditionaly redact the reviewboard page

--- reviewboard.js
+++ reviewboard.js
@@ -1,8 +1,21 @@
 "use strict";
 
-function init() {
+async function storage(id, visible) {
+  if (visible != null) {
+    return browser.storage.sync.set({[id]: visible | 0});
+  }
+  return (await browser.storage.sync.get(String(id)))[id];
+}
+
+async function init() {
   const meta = document.querySelector(".review-request-meta");
-  meta.firstChild.textContent = "";
+  const author = document.querySelector("#mozreview-review-header>a+a");
+  const bug_id = author.textContent.match(/\d+/)[0];
+  const visible = await storage(bug_id);
+
+  if (!visible) {
+    meta.firstChild.textContent = "[redacted]";
+  }
 }
 
 init();
